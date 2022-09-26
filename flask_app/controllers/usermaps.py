d3fb6fdf037c52bd05a0050f4023c0fe2c0bc871
Iain Donald <iaindonald@outlook.com>
2022-09-26 12:55:04 -0700

    AddNewUserToJSON

--- flask_app/controllers/usermaps.py
+++ flask_app/controllers/usermaps.py
@@ -2,10 +2,11 @@ from flask_app import app
 from flask import render_template,redirect,request,session,flash
 from flask_app.models.usermap import Usermap
 from flask_app.controllers import dbtalk
+import json, random
 
 """    ROUTES    """
 
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+@app.route('/')
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
@@ -16,8 +17,6 @@ def hello(name):
 
 @app.route('/map')                           
 def map():
-    # Instead of returning a string, 
-    # we'll return the result of the render_template method, passing in the name of our HTML file
     return render_template('map.html')
 
 @app.route('/testcenter/<id>/<userID>')
@@ -45,12 +44,38 @@ def loginPOST():
 
 @app.route("/new", methods=['POST', 'GET'])
 def new():
+
+    #get object to be transferred to JSON for ds.json
     data = {
         "firstName": request.form.get("first_name"),
         "lastName" : request.form.get("last_name"),
         "email" : request.form.get("email"),
-        "pw" : request.form.get("password")
+        "pw" : request.form.get("password"),
+        "userID": str(random.randrange(1, 11000))
     }
+
+    conf = request.form.get("conf")
+
+    #check if password and password confirmation are the same
+    if(conf == data['pw']):
+        print("TRUE")
+    else:
+        print("FALSE")
+
+    """implement check for if userID exists and assign new userID"""
+
+    y = json.dumps(data)
+
+    print(y)
+
+    #insert object to JSON file
+
+    with open('flask_app\controllers\db.json','r+') as file:
+        file_data = json.load(file)
+        file_data["Users"].append(data)
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
+
     return render_template('login.html')
 
 
